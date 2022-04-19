efad5790bd0c85e684addb2cfae167ad2483b690
akarat27 <akrs33@gmail.com>
2022-04-19 12:22:41 +0700

    Dev: add function of key install ,and clear table

--- Apps/lockertable.py
+++ Apps/lockertable.py
@@ -1,3 +1,35 @@
+import sys
 from Minor.DB import dbclient
 
-dbclient.createTable()
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print('Please input the option')
+    else:
+        option = sys.argv[1]
+        if option == "create":
+            option2 = sys.argv[2]
+            if option2 == "locker":
+                dbclient.createTable()
+                print('Table Locker created.')
+            elif option2 == "key":
+                dbclient.createTableKey()
+                print('Table Key created.')
+            else:
+                print('No Table found.')
+        elif option == "setup":
+            option2 = sys.argv[2]
+            if option2 == "key":
+                option3 = sys.argv[3]
+                saved = (str(option3))
+                # print(saved)
+                dbclient.createKey(saved)
+                print('Key inserted.')
+            else:
+                print('No action.')
+        elif option == "clear":
+            option2 = sys.argv[2]
+            if option2 == "key":
+                dbclient.clearKey()
+                print('All Key(s) removed')
+        else:
+            print('Invalid input of option')
